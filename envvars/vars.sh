a003a8fb2529eb96c2d1df3ce98f283a85edbb78
Edisson Zuniga <giovanemere@gmail.com>
2023-03-17 22:20:05 -0500

    add certficitaes rancher

---
+++ envvars/vars.sh
@@ -0,0 +1,9 @@
+# Variables Dinamicas
+###################################################################################
+
+      dominio="$1"  #rancher.yourdomain.com
+
+# Variables fijas
+
+      folderPath="$HOME/Certificados"
+      varCN="*.$dominio"
